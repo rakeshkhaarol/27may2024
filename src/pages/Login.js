67f5ac02a24c2b1b2230474f42ae2be92cbc0092
rakeshkhaarol <kharol64666@gmail.com>
2024-05-28 11:05:08 +0530

    c3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <>
+      <div>
+        <h1 style={{ textAlign: 'center' }}> Login The Information</h1>
+
+        <Form style={{ marginLeft: 580, marginTop: 100 }} className='w-25'>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="button">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </>
+
+
   )
 }
